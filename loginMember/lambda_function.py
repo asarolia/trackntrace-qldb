6e938d59815d45eb8106a5818ca3916cf97f45ff
Abhishek Sarolia <saroli@amazon.com>
2022-12-18 16:20:30 +0530

    Updated code for API integration flow during login

--- loginMember/lambda_function.py
+++ loginMember/lambda_function.py
@@ -6,7 +6,8 @@ from boto3 import client
 
 from common import create_qldb_driver
 from pyqldb.driver.qldb_driver import QldbDriver
-from base64 import encode, decode
+#from base64 import encode, decode
+import json
 
 qldb_client = client('qldb')
 
@@ -37,12 +38,13 @@ def select_document(driver, table_name, username , password):
     
     for doc in cursor:
         db_username = doc['Username']
-        db_password = doc['Password'].decode('ascii')
+        db_password = doc['Password']
+        # db_password = doc['Password'].decode('ascii')
         db_memeberid = doc['MemberDocId']
         db_usertype = doc['Usertype']
     
     # validate credentials 
-    if db_username == username and db_password == password.decode('ascii'):
+    if db_username == username and db_password == password: #password.decode('ascii'):
         login_status = True
         user_type = db_usertype
         document_id = db_memeberid
@@ -57,13 +59,33 @@ def lambda_handler(event, context):
     Process values from Event payload
     """
     
-    ledger_name = event.get('ledgername')
-  
-    username = event.get('username')
-    password = event.get('password')
-   
+    API_flow = False
+    processing_error = False
+    return_msg = ''
+    response = {}
+    
+    if event.get('body') is None:
+        # pick from lambda test payload
+        #print("lambda test flow")
+        ledger_name = event.get('ledgername')
+        username = event.get('username')
+        password = event.get('password')
+
+        #print(ledger_name)
+    else:
+        API_flow = True
+        # pick from API request
+        # print("API integration flow")
+        body_dict_payload = json.loads(event.get('body'))
+        ledger_name = body_dict_payload.get('ledgername')
+        username = body_dict_payload.get('username')
+        # pick base 64 encoded pwd from payload 
+        password = body_dict_payload.get('password')
+        # password = body_dict_payload.get('password').encode('ascii')
+        # print(ledger_name)
 
     print('Started processing of login request ..')
+    
     
     
     try:
@@ -73,29 +95,30 @@ def lambda_handler(event, context):
             
             if login_status:
                 
-                return {
+                response = {
                     "isBase64Encoded": "false",
                     "statusCode": 200,
-                    "body": {
+                    "body": json.dumps({
                         "message": "Login success",
                         "Usertype" : user_type,
                         "MemberId" : doc_id
-                    }
+                    })
                 }
             else:
-                return {
+                response = {
                     "isBase64Encoded": "false",
                     "statusCode": 403,
-                    "body": {
+                    "body": json.dumps({
                         "message": "Login Failed"
-                    }
+                    })
                 }            
             
     except Exception as e:
         print('Error processing request {}',format(e))
-        return {
+        response = {
             "isBase64Encoded": "false",
             "statusCode": 500,
             "body": 'Error processing request - '+ format(e)
         }
     
+    return response
